808fc0849c09d9bbc41dbd6717e892bac166f770
PaulaaGr <pngonzalez@uniboyaca.edu.co>
2023-04-22 23:00:51 -0500

    Final ejercicio 6

--- aplicacion6/buscarTexto.js
+++ aplicacion6/buscarTexto.js
@@ -1,12 +1,16 @@
-function Coincidencias(texto, buscar) {
+function igual(texto, buscar) {
+
   let regex = new RegExp(buscar, 'gi');
   return texto.replace(regex, match => "<font color=red>"+match+"</font>");
+
 }
 
 function buscar() {
+
   let parrafo = document.getElementById("parrafo").value;
   let texto = document.getElementById("texto").value;
-  let algo = Coincidencias(parrafo, texto);
+  let algo = igual(parrafo, texto);
 
   document.getElementById('respuesta').innerHTML = algo;
+  
 }

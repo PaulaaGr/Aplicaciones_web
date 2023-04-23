32103d49d6817d53fd99b2648910d7414bf32c0f
PaulaaGr <pngonzalez@uniboyaca.edu.co>
2023-04-22 22:05:46 -0500

    Ultimos detalles ejercicio 3

--- aplicacion3/datos.js
+++ aplicacion3/datos.js
@@ -4,16 +4,23 @@ function procedimiento() {
     var edad = document.getElementById('edad').value;
 
     for (let index = 0; index < 1; index++) {
+
         if (edad >= 18) {
             document.getElementById('area').value = nombre+" es mayor de edad";
             document.getElementById('nombre');
             document.getElementById('edad');
 
-        } else {
+        } else if (edad == 0){
+            document.getElementById('area').value = nombre+" no es una edad válida";
+            document.getElementById('nombre');
+            document.getElementById('edad');
+        }else{
             document.getElementById('area').value = nombre+" es menor de edad";
             document.getElementById('nombre');
             document.getElementById('edad');
         }
+    
+        
 
     }
 }
